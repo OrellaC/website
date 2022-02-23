12c72c87f6bdded2d4d80ccaba1ee69b72d692ef
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-22 23:00:24 -0500

    Started Uploading my resume

---
+++ src/pages/Resume/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h1>Experience</h1>
+
+            <h3>Per Scholas</h3>
+            <h3>Software Engineering Student</h3>
+            <h3>November 2021 - March 2022</h3>
+            <ul>
+                <li>Implement the fundamentals of Javascript ES6, HTML5, CSS3, React.js, and Java through the creation of multiple projects</li>
+                <li>Debug software errors in order to create functional web applications </li>
+                <li>Write and deploy clean and efficient code </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Resume;
